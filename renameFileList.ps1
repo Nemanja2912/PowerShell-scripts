c8797231b6437beaf3b7d5e34566b62be0786154
Nemanja Mijailović <48948445+Nemanja2912@users.noreply.github.com>
2022-11-02 15:07:44 +0100

    Update renameFileList.ps1

--- renameFileList.ps1
+++ renameFileList.ps1
@@ -1,9 +1,10 @@
-﻿$path = "C:\VariableChallenge"
+$path = "C:\Users\neman\downloads\images"
 $fileList = Get-ChildItem -Path $Path
 $counter = 0
 $fileName = Read-Host "Insert name for file"
+$extension = Read-Host "Insert extension for file"
 
 foreach($file in $fileList){
-    Rename-Item $file -NewName "$fileName $counter"
+    Rename-Item $file -NewName "$fileName$counter.$extension"
     $counter++
 }
